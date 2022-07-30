dc940594e3eb870a719be8d9af9adebbd39b465f
ogabek2606 <110012533+ogabek2606@users.noreply.github.com>
2022-07-30 03:10:30 -0400

    Add files via upload

---
+++ og`abek 88/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 1, b = 8;
+
+        if (a > b) {
+            System.out.println (b);
+        } else if ( b > a) {
+            System.out.println (a);
+        }else {
+            System.out.println ("teng");
+        }
+
+    }
+}
